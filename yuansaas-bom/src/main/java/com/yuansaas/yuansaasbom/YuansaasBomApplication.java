d2dedaba78c7af2ca14422620f1c47dbd20529b7
劉皛旭 <77199452+liuxiaoxu97@users.noreply.github.com>
2025-12-22 18:28:35 +0800

    feat: 测试

---
+++ yuansaas-bom/src/main/java/com/yuansaas/yuansaasbom/YuansaasBomApplication.java
@@ -0,0 +1,13 @@
+package com.yuansaas.yuansaasbom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuansaasBomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuansaasBomApplication.class, args);
+    }
+
+}
---
+++ yuansaas-bom/src/test/java/com/yuansaas/yuansaasbom/YuansaasBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuansaas.yuansaasbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuansaasBomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
